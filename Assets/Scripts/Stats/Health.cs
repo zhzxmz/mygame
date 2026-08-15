f4a551ff001d7e3c13af711fe1ea2635a5d91b58
zhzxmz <163139455+zhzxmz@users.noreply.github.com>
2026-08-15 08:48:48 +0800

    Add inventory item lookup and slot auto references

--- Assets/Scripts/Stats/Health.cs
+++ Assets/Scripts/Stats/Health.cs
@@ -1,38 +1,37 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Health : MonoBehaviour{
-public int maxHP;
-public int currentHP;
-public event Action<int,int> OnHealthChanged;
-public event Action OnDeath;
+public class Health : MonoBehaviour
+{
+    public int maxHP;
+    public int currentHP;
+    public event Action<int, int> OnHealthChanged;
+    public event Action OnDeath;
 
-    // Start is called before the first frame update
+    private bool isDead;
+
     void Start()
     {
-        currentHP=maxHP;
+        currentHP = maxHP;
     }
 
-    // Update is called once per frame
-    
-        public void TakeDamage(int Damage)
+    public void TakeDamage(int Damage)
     {
-        currentHP-=Damage;
-        if(currentHP<0)currentHP=0;
-        OnHealthChanged?.Invoke(currentHP,maxHP);
-        if(currentHP<=0)Die();
+        if (isDead) return;
+
+        currentHP -= Damage;
+        if (currentHP < 0) currentHP = 0;
+        OnHealthChanged?.Invoke(currentHP, maxHP);
+        if (currentHP <= 0) Die();
     }
+
     void Die()
     {
+        if (isDead) return;
+
+        isDead = true;
         Debug.Log("DIE");
         OnDeath?.Invoke();
-        GetComponent<EnemyDrop>().Drop();
         Destroy(gameObject);
     }
-    
-
-    
-    
 }
